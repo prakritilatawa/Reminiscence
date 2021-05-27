3fd67bee6c23e365ec015b9898b1067f522b157b
Prakriti Latawa <prakriti.latawa110@gmail.com>
2021-05-27 19:56:04 +0530

    Authentication

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { AppBar, Avatar, Toolbar, Typography, Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
-
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
  import useStyles from './styles';
 import a from '../../images/a.png';
 
 const Navbar = () => {
     const classes = useStyles();
-    const user = null;
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const location = useLocation();
+
+    const logout = () => {
+        dispatch({type: 'LOGOUT'});
+
+        history.push('/');
+        setUser(null);
+
+    };
+
+
+    useEffect(() => {
+        const token = user?.token;
+        setUser(JSON.parse(localStorage.getItem('profile')));
+    }, [location]);
 
 
     return (
@@ -23,11 +40,11 @@ const Navbar = () => {
               <div className={classes.profile}>
                  <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
                  <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                 <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                 <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
               </div>
 
           ) : (
-              <Button component={Link} to=" /auth" variant="contained" color="primary">Sign In</Button>
+              <Button component={Link} to = "/auth" variant="contained" color="primary">Sign In</Button>
 
           )}
 
